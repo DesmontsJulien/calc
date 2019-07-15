df1f8dfa72be5dec6f9ae05168afed7a3ed59e4b
DesmontsJulien <juliendesmonts@yahoo.fr>
2019-07-15 15:50:39 +0200

    Add new function + update changelog + add new test

---
+++ calc.js
@@ -0,0 +1,43 @@
+function add(x, y){
+    math.import({
+        myConstant: 42,
+        hello: function (name) {
+          return 'hello, ' + name + '!'
+        }
+      })
+      
+}
+
+function subtract(x, y){
+    math.import({
+        myConstant: 42,
+        hello: function (name) {
+          return 'hello, ' + name + '!'
+        }
+      })
+      
+}
+
+function multiply(x, y){
+    math.import({
+        myConstant: 42,
+        hello: function (name) {
+          return 'hello, ' + name + '!'
+        }
+      })
+      
+}
+
+function divide(x, y){
+    math.import({
+        myConstant: 42,
+        hello: function (name) {
+          return 'hello, ' + name + '!'
+        }
+      })
+      
+}
+
+function abs(x){
+    
+}
